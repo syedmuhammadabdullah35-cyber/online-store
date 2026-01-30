c484456f4ba4dc8ebeecd01eba1dbb56c299aa10
Syed Abdullah <syedmuhammadabdullah35@gmail.com>
2026-01-30 15:55:38 +0500

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -13,6 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 999;
+app.use(cors());
 
 // --- DIRECTORY SETUP ---
 const uploadDir = path.join(__dirname, 'uploads');
